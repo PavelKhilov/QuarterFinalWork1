ee6317cc3b671ba40a6651ec09ef4b24212dcafc
Pavel <khilov.pavel@eyandex.ru>
2023-01-29 20:27:59 +0300

    Method CountElementLength

--- Program.cs
+++ Program.cs
@@ -3,3 +3,12 @@
  for(int i = 0; i < array.Length; i++)
     Console.Write('"' + array[i] + '"' + " ");
 }
+
+int CountElementLenght(string[] array, int length)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+        if(array[i].Length <= length)
+            count++;
+    return count;
+}
