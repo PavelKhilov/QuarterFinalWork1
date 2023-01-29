76f8dc56485c9181a1942e3ac563e780a7a55fb3
Pavel <khilov.pavel@eyandex.ru>
2023-01-29 20:30:01 +0300

    Full program

--- Program.cs
+++ Program.cs
@@ -23,3 +23,32 @@ void CopySizeElements(string[] array1, string[] array2, int size)
             j++;
         }
 }
+
+string[] array1 = {"hello", "2", "world", ":-)"};
+string[] array2 = {"1234", "1567", "-2", "computer sience"};
+string[] array3 = {"Russia", "Denmark", "Kazan"};
+
+ShowArray(array1);
+Console.WriteLine();
+ShowArray(array2);
+Console.WriteLine();
+ShowArray(array3);
+Console.WriteLine();
+
+Console.Write("Input of maximum element size: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] newArray1 = new string[CountElementLenght(array1, size)];
+CopySizeElements(array1, newArray1, size);
+ShowArray(newArray1);
+Console.WriteLine();
+
+string[] newArray2 = new string[CountElementLenght(array2, size)];
+CopySizeElements(array2, newArray2, size);
+ShowArray(newArray2);
+Console.WriteLine();
+
+string[] newArray3 = new string[CountElementLenght(array3, size)];
+CopySizeElements(array3, newArray3, size);
+ShowArray(newArray3);
+Console.WriteLine();
